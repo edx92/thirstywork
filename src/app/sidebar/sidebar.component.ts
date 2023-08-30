c13e79fcaeb1705fb089ab63235dfa1d6c905829
Ed Cross <ed@edx92-design.co.uk>
2023-08-30 09:29:25 +0100

    Reorganised styles of flex cards and added add plant card to cards component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faSeedling, faPuzzlePiece, faUserGear,faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { faSeedling, faLocationDot, faUserGear,faDroplet } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,7 +9,7 @@ import { faSeedling, faPuzzlePiece, faUserGear,faDroplet } from '@fortawesome/fr
 export class SidebarComponent {
   icons = {
     faSeedling:faSeedling,
-    faPuzzlePiece:faPuzzlePiece,
+    faLocationDot:faLocationDot,
     faUserGear:faUserGear,
     faDroplet:faDroplet
   }
